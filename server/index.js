ebea62017edd12349df62fa9524166fdd1e9103c
Priyanshu <22BCS13942@cuchd.in>
2026-05-03 14:22:01 +0530

    final deployment setup

--- server/index.js
+++ server/index.js
@@ -1,23 +1,38 @@
-require("dotenv").config();  
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
-console.log("JWT:", process.env.JWT_SECRET);
-// Routes
+
+
 app.use("/auth", require("./routes/auth"));
 app.use("/projects", require("./routes/projects"));
 app.use("/tasks", require("./routes/tasks"));
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(" DB Error:", err));
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.log("❌ DB Error:", err));
+
+
+const frontendPath = path.join(__dirname, "../client/dist");
+
+app.use(express.static(frontendPath));
+
+app.use((req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
